223976896a195ac7501e9ca387c45f08b534e620
shivani-520 <shivani_2002@icloud.com>
2025-02-16 16:52:10 +0000

    Door Interactable
    Open / Close door

---
+++ Assets/Scripts/Interact System/Interactables/Door.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private Quaternion targetRotation;
+    private bool isRotating = false;
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        targetRotation = transform.rotation * Quaternion.Euler(0, -90f, 0);
+    }
+
+    private void Update()
+    {
+        if (isRotating)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            if(transform.rotation == targetRotation)
+            {
+                isRotating = false;
+            }
+        }
+    }
+
+    public void Interact()
+    {
+        if(!isRotating)
+        {
+            if (isOpen)
+            {
+                targetRotation = transform.rotation * Quaternion.Euler(0f, 90f, 0f);
+            }
+            else
+            {
+                targetRotation = transform.rotation * Quaternion.Euler(0f, -90f, 0f);
+            }
+
+            isRotating = true;
+            isOpen = !isOpen;
+        }
+
+        Debug.Log("Interacting with: " + transform.name);
+    }
+}

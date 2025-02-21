1d7e0302dd7b8f983058e5ac43072b6f43e93292
Kieran Speight <kieranjspeight@gmail.com>
2025-02-21 20:07:34 +0000

    Interaction UI
    UI element is activated when the player is aiming at an interactable object.

--- Assets/Scripts/Interact System/InteractSystem.cs
+++ Assets/Scripts/Interact System/InteractSystem.cs
@@ -1,22 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractSystem : MonoBehaviour
 {
     [SerializeField] private Transform interactSource;
     [SerializeField] private float interactRange;
+    public GameObject interactUI;
 
     private void Update()
     {
+        Ray ray = new Ray(interactSource.position, interactSource.forward);
+
         if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = new Ray(interactSource.position, interactSource.forward);
-            
+        {   
             if(Physics.Raycast(ray, out RaycastHit hitInfo, interactRange))
             {
                 if(hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactable))
                 {
                     interactable.Interact();
                 }
+            }
+        }
+
+        interactUI.SetActive(false);
+
+        if(Physics.Raycast(ray, out RaycastHit hoverInfo, interactRange))
+        {
+            if(hoverInfo.collider.gameObject.CompareTag("Interactable"))
+            {
+                interactUI.SetActive(true);
             }
         }
     }

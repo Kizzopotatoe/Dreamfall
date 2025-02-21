44b5d0d02f2ec5a8c74e7cd27872596dcdcd00f0
Kieran Speight <kieranjspeight@gmail.com>
2025-02-21 15:03:44 +0000

    Dream Blockout p2

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -42,7 +42,7 @@ public class MainMenuManager : MonoBehaviour
 
     private void PlayGame()
     {
-        SceneManager.LoadScene("Bedroom");
+        SceneManager.LoadScene("Dream");
     }
 
     private void ExitGame()

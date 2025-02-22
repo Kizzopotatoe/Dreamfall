ab96614681faf70f9ab7f79158b53caa8d9ee53e
Kieran Speight <kieranjspeight@gmail.com>
2025-02-22 16:43:55 +0000

    Space Whale
    He spins pretty fast but aside from that he chill.

---
+++ Assets/Scripts/Environment/SpaceWhale.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpaceWhale : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    public Transform[] target;
+    private int targetNumber = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var step = speed * Time.deltaTime; 
+        float rotateStep = speed * Time.deltaTime;
+        Vector3 targetDirection = target[targetNumber].position - transform.position;
+
+        transform.position = Vector3.MoveTowards(transform.position, target[targetNumber].position, step);
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, rotateStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+        if (Vector3.Distance(transform.position, target[targetNumber].position) < 0.001f)
+        {
+            if(targetNumber == 8) return;
+            targetNumber++;
+        }
+    }
+}
